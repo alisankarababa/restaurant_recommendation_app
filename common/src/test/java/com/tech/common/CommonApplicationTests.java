8ca64f5f9e9e403045ac79a29de12ca5eb7dce56
Alişan <alisankarababa@gmail.com>
2024-03-19 13:52:57 +0300

    first commit

---
+++ common/src/test/java/com/tech/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
